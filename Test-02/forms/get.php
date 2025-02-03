804d46812e09de5ee2c936cf2b27115e0c9bd105
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2025-02-03 15:00:24 -0500

    test

---
+++ Test-02/forms/get.php
@@ -0,0 +1,9 @@
+<?php
+
+$fname = $_GET["fname"]:
+$hero = $_GET["hero"]:
+
+echo("Welcome ".$fname."!");
+echo("Your favorite superhero is: ".$hero);
+
+?>
---
+++ Test-02/forms/post.php
@@ -0,0 +1,9 @@
+<?php
+
+$fname = $_POST["fname"]:
+$hero = $_POST["hero"]:
+
+echo("Welcome ".$fname."!");
+echo("Your favorite cartoon character is: ".$hero);
+
+?>
